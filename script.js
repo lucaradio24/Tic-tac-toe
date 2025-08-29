c2868ae467ad6886357b3ec55fc18918c5985384
lucaradio24 <lukaabeck3@gmail.com>
2025-08-29 19:35:24 +0200

    Added minimal UI

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+// UI
+const cells = document.querySelectorAll('.cell')
+const status = document.querySelector('.status')
+const restartBtn = document.querySelector('#restart')
+const modal = document.querySelector('#playerModal')
+const form = document.querySelector('#playerForm')
+const nameX = document.querySelector('#nameX')
+const nameO = document.querySelector('#nameO')
+
+modal.showModal()
+
+function render(){
+    let state = GameController.getGameState();
+    cells.forEach(function (cell){
+        const index = cell.dataset.index;
+        cell.textContent = state.board[index];
+        cell.disabled = state.board[index] !=='' || state.isOver
+        })
+        
+        if(state.isOver && state.lastResult.status === 'win'){
+           status.textContent = `Ha vinto ${state.lastResult.winner} -> ${state.current.name}` 
+        } else if (state.isOver && state.lastResult.status === 'draw'){
+            status.textContent = 'Pareggio'
+        } else {
+            status.textContent = `Tocca a ${state.current.name} (${state.current.mark}) `
+        }
+    }
+
+cells.forEach( cell => {     // cells è un array, per ogni elemento (cell) di questo array c'è un event listener
+    cell.addEventListener('click', () =>{
+        const index = Number(cell.dataset.index);  // quando c'è il click index legge il data-index del bottone
+        GameController.makeMove(index); // qui passiamo questo index nella funzione
+        render() // disegniamo la griglia
+    })
+})
+
+restartBtn.addEventListener('click', () => {
+    modal.showModal()
+    GameController.init(p1, p2);
+    render();
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nameXValue = nameX.value.trim() // per togliere gli spazi iniziali finali
+    const nameOValue = nameO.value.trim()
+    const p1 = createPlayer(nameXValue || 'Player X', 'X');
+    const p2 = createPlayer(nameOValue || 'Player O', 'O');
+    GameController.init(p1, p2);
+    modal.close();
+    render()
+})
+
+// GAME LOGIC
+
 const WIN_LINES = [
   [0, 1, 2],
   [3, 4, 5], // righe orizzontali
@@ -84,6 +139,7 @@ const GameController = (function () {
     currentPlayer = pX; // inizia sempre il giocatore X
     movesCount = 0; // azzera il contatore delle mosse
     Gameboard.resetBoard(); // pulisce la board
+    modal.showModal()
   };
 
   const getCurrentPlayer = function () {
@@ -129,7 +185,7 @@ const GameController = (function () {
     // ritorna lo stato completo del gioco
     const board = Gameboard.getBoard(); // prende l'array delle celle
     const current = getCurrentPlayer(); // prende il giocatore corrente
-    return { board, current, movesCount, lastResult }; // ritorna un oggetto con tutte le informazioni
+    return { board, current, movesCount, lastResult, isOver }; // ritorna un oggetto con tutte le informazioni
   };
 
   return { getCurrentPlayer, makeMove, init, getGameState }; // esporta le funzioni pubbliche del modulo
@@ -140,15 +196,20 @@ function createPlayer(name, mark) {
   return { name, mark }; // ritorna un oggetto con nome e simbolo
 }
 
-const p1 = createPlayer("Luca", "X"); // crea il giocatore X
-const p2 = createPlayer("Giorgia", "O"); // crea il giocatore O
-GameController.init(p1, p2); // inizializza il gioco con i due giocatori
-GameController.getGameState(); // ottiene lo stato iniziale del gioco
 
-GameController.makeMove(0); // giocatore X mette il simbolo nella cella 0
-GameController.getGameState(); // controlla lo stato dopo la prima mossa
-GameController.makeMove(4); // giocatore O mette il simbolo nella cella 4
-GameController.makeMove(1); // giocatore X mette il simbolo nella cella 1
-GameController.makeMove(8); // giocatore O mette il simbolo nella cella 8
-GameController.makeMove(2); // giocatore X mette il simbolo nella cella 2 (riga vincente!)
-console.log(GameController.getGameState()); // stampa il risultato finale
+
+
+GameController.init(p1, p2); // inizializza il gioco con i due giocatori
+
+
+
+render()
+console.log(GameController.getGameState()); // ottiene lo stato iniziale del gioco
+
+// GameController.makeMove(0); // giocatore X mette il simbolo nella cella 0
+// GameController.getGameState(); // controlla lo stato dopo la prima mossa
+// GameController.makeMove(4); // giocatore O mette il simbolo nella cella 4
+// GameController.makeMove(1); // giocatore X mette il simbolo nella cella 1
+// GameController.makeMove(8); // giocatore O mette il simbolo nella cella 8
+// GameController.makeMove(2); // giocatore X mette il simbolo nella cella 2 (riga vincente!)
+// console.log(GameController.getGameState()); // stampa il risultato finale
